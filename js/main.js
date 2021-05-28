0de362cb2020fada6a6567505581fdcd713b86af
strawberryoolongtea <strawberrycream@kakao.com>
2021-05-29 08:54:30 +0900

    Add visual animation

--- js/main.js
+++ js/main.js
@@ -36,3 +36,11 @@ window.addEventListener('scroll', _.throttle(function () {
 }, 300));
 
 // _.throttle(함수, 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.8...
+    opacity: 1
+  });
+});
