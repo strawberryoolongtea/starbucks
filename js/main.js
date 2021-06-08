c3f53e228fc771e2f7f2b886290c7e17a18ad356
strawberryoolongtea <strawberrycream@kakao.com>
2021-06-09 01:05:27 +0900

    Add awards slide section

--- js/main.js
+++ js/main.js
@@ -68,7 +68,17 @@ new Swiper('.promotion .swiper-container', {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next'
   }
-})
+});
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next'
+  }
+});
 
 const promotionEl = document.querySelector('.promotion')
 const promotionToggleBtn = document.querySelector('.toggle-promotion')
